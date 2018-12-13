e753a6a5be27eb57aff1da29ea0c49ad68da8f66
daniel.kesner@nike.com <daniel.kesner@nike.com>
2018-12-13 11:05:01 -0800

    Standardize naming of HTTP model classes

--- src/main/java/model/http/HttpExchangeMetadata.java
+++ src/main/java/model/http/HttpExchangeMetadata.java
@@ -11,10 +11,10 @@ public class HttpExchangeMetadata {
     private String requestMethod;
     private String requestUrl;
     private String requestBody;
-    private String responsePayload;
+    private String responseBody;
     private int status;
 
-    public HttpExchangeMetadata(String requestMethod, String requestUrl, String responsePayload, int status) {
-        this(requestMethod, requestUrl, null, responsePayload, status);
+    public HttpExchangeMetadata(String requestMethod, String requestUrl, String responseBody, int status) {
+        this(requestMethod, requestUrl, null, responseBody, status);
     }
 }
